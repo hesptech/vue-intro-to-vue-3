ba0b17cfe5f0b3e6204537cb1ac62f2a671dc41f
oswaldo olea <o.olea@sintraconsulting.com>
2021-12-08 11:51:19 +0100

    computed properties

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            onSale: true,
+            //image: './assets/images/socks_green.jpg',
             url: 'https://www.google.com',
-            inStock: true,
+            //inStock: true,
             inventory: 9,
             details: ['50% cotton', '25% wool', '25%polyster'],
             variants: [
-                { id: 22, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 23, color: 'blue', image: './assets/images/socks_blue.jpg'}
+                { id: 22, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 23, color: 'blue', image: './assets/images/socks_blue.jpg', qunatity: 0}
             ]
         }
     },
@@ -21,8 +24,25 @@ const app = Vue.createApp({
         updateImage(variantImage) {
             this.image = variantImage
         },
+        updateVariant(index ) {
+            this.selectedVariant = index
+        },
         substarctFromCart() {
             if(this.cart >= 1)this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSaleBadge() {
+            return this.onSale
         }
     }
 })
